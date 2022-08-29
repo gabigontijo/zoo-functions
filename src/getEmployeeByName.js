8516a6b1ca5681b17948f03ac3cb6faa0b0cedb8
Gabriela Gontijo <dev.gabigontijo@gmail.com>
2022-08-29 21:53:52 +0100

    requesito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
+  if (employeeName === undefined) {
+    return {};
+  }
+  return data.employees.filter((indice) =>
+    indice.firstName === employeeName || indice.lastName === employeeName)[0];
   // seu código aqui
 }
-
+console.log(getEmployeeByName('Nigel'));
 module.exports = getEmployeeByName;
