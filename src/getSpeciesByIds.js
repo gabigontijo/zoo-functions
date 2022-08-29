165ede6768d635f0df35de9fa9cb8b321acb07c1
Gabriela Gontijo <dev.gabigontijo@gmail.com>
2022-08-29 20:13:58 +0100

    requesito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,35 @@
-const data = require('../data/zoo_data');
+const data = require("../data/zoo_data");
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) {
+    return [];
+  }
+  if (ids.length >= 1) {
+    return data.species.filter((index) =>
+      ids.some(id => index.id === id));
+
+    // let resultado = [];
+    // ids.forEach((id) => {
+    //   let filtrado = data.species.filter((specie) => {
+    //     return specie.id === id;
+    //   })
+    //   if (filtrado.length > 0){
+    //     resultado.push(filtrado[0]);
+    //   }
+    // });
+    // return resultado;
+  }
+  // else {
+  //   return data.species.filter((animal, indice ) => animal.id ===ids[indice]);
+
+  // }
 }
+
+console.log(
+  getSpeciesByIds(
+    "e8481c1d-42ea-4610-8e11-1752cfc05a46",
+    "0938aa23-f153-4937-9f88-4858b24d6bce"
+  )
+);
 
 module.exports = getSpeciesByIds;
