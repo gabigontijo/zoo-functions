b7c5e1e801ecd708663340d9311ae439af15cfb0
Gabriela Gontijo <dev.gabigontijo@gmail.com>
2022-08-30 23:09:18 +0100

    requesito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,19 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((children) => children.age < 18).length;
+  const adult = entrants.filter((adults) => adults.age >= 18 && adults.age < 50).length;
+  const senior = entrants.filter((seniors) => seniors.age >= 50).length;
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length < 1) {
+    return 0;
+  }
+  const cont = countEntrants(entrants);
+  return (cont.child * prices.child) + (cont.adult * prices.adult) + (cont.senior * prices.senior);
 }
 
 module.exports = { calculateEntry, countEntrants };
