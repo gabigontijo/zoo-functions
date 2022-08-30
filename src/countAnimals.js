f0e8f0691ddc91c16c5deacaebd2269f0e32e2bd
Gabriela Gontijo <dev.gabigontijo@gmail.com>
2022-08-30 16:57:46 +0100

    requesito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const residentes = {};
+    data.species.forEach((objectAnimal) => {
+      residentes[objectAnimal.name] = objectAnimal.residents.length;
+    });
+    return residentes;
+  }
+  if (Object.keys(animal).length === 1) {
+    const findAnimal = data.species.find((witchAnimal) => (
+      witchAnimal.name === Object.values(animal)[0]
+    ));
+    console.log(findAnimal);
+    return findAnimal.residents.length;
+  }
+  const findAnimalSexo = data.species.find((witchAnimal) => (
+    witchAnimal.name === Object.values(animal)[0]
+  ));
+  return findAnimalSexo.residents.filter((sexo) => sexo.sex === Object.values(animal)[1]).length;
 }
+console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,9 +1,12 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  let gerentes = {};
-  data.employees.forEach((employee) => (employee.managers.forEach((managerId) =>
-  gerentes[managerId] = managerId)));
+  const gerentes = {};
+  data.employees.forEach((employee) => {
+    employee.managers.forEach((managerId) => {
+      gerentes[managerId] = managerId;
+    });
+  });
   console.log(gerentes);
   if (gerentes[id] !== undefined) {
     return true;
@@ -13,18 +16,13 @@ function isManager(id) {
 
 function getRelatedEmployees(managerId) {
   if (isManager(managerId)) {
-    const manager = data.employees.filter((indice) => indice.id === managerId)[0];
-    // console.log(employee);
-    const filterEmployee = Object.values(manager.managers);
-    // console.log(filterEmployee);
-    return data.employees.filter((ids) => filterEmployee.some((nameId) => ids.id === nameId))
-      .map((employee) => (`${employee.firstName} ${employee.lastName}`));
+    const manager = data.employees.filter((employee) => employee.managers.some((someManager) =>
+      someManager === managerId));
+    console.log(manager);
+    return manager.map((employee) => (`${employee.firstName} ${employee.lastName}`));
   }
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-// data.species.filter((index) =>
-//   ids.some((id) => index.id === id));
-// seu código aqui}
 
-console.log(isManager('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
