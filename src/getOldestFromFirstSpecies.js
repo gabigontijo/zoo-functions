655f4206a087a588df65bf6812b020e1bb07bad8
Gabriela Gontijo <dev.gabigontijo@gmail.com>
2022-08-31 00:16:02 +0100

    requesito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,21 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+ const funcionario = employees.find((func) => func.id === id);
+ console.log(funcionario);
+ const firstAnimal = funcionario.responsibleFor[0];
+ console.log(firstAnimal);
+ const firstSpecie = species.find((specie) => specie.id === firstAnimal).residents;
+ console.log(firstSpecie);
+ let old = {age: 0};
+ firstSpecie.forEach((older) => {
+ if (older.age > old.age) {
+  old = older;
+   }})
+ return [old.name, old.sex, old.age];
+
 }
 
+console.log(getOldestFromFirstSpecies('c1f50212-35a6-4ecd-8223-f835538526c2'));
 module.exports = getOldestFromFirstSpecies;
