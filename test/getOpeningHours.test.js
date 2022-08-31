bcb55f5c0d53d1a8eb40a2943142bb3adf6952fc
Gabriela Gontijo <dev.gabigontijo@gmail.com>
2022-08-31 16:20:56 +0100

    requesito testst

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -18,23 +18,23 @@ describe('Testes da função getOpeningHours', () => {
     expect(getOpeningHours('Wednesday', '09:00-PM')).toEqual('The zoo is closed');
   });
 
-  it(' Para os argumentos `Thu` e `09:00-AM` deve lançar uma exceção com a mensagem: The day must be valid. Example: Monday', () => {
-    expect(getOpeningHours('Thu', '09:00-AM')).toThrow(Error('The day must be valid. Example: Monday'));
+  it('Para os argumentos `Thu` e `09:00-AM` deve lançar uma exceção com a mensagem: The day must be valid. Example: Monday', () => {
+    expect(() => getOpeningHours('Thu', '09:00-AM')).toThrow('The day must be valid. Example: Monday');
   });
 
   it('Para os argumentos `Friday` e `09:00-ZM` deve lançar uma exceção com a mensagem: The abbreviation must be \'AM\' or \'PM\'', () => {
-    expect(getOpeningHours('Friday', '09:00-ZM')).toThrow(Error('The abbreviation must be \'AM\' or \'PM\''));
+    expect(() => getOpeningHours('Friday', '09:00-ZM')).toThrow('The abbreviation must be \'AM\' or \'PM\'');
   });
 
   it('Para os argumentos `Saturday` e `C9:00-AM` deve lançar uma exceção com a mensagem: The hour should represent a number', () => {
-    expect(getOpeningHours('Saturday', 'C9:00-AM')).toThrow(Error('The hour should represent a number'));
+    expect(() => getOpeningHours('Saturday', 'C9:00-AM')).toThrow('The hour should represent a number');
   });
 
   it('Para os argumentos `Monday` e `13:00-AM` deve lançar uma exceção com a mensagem: The hour must be between 0 and 12', () => {
-    expect(getOpeningHours('Monday', '13:00-AM')).toThrow(Error('The hour must be between 0 and 12'));
+    expect(() => getOpeningHours('Monday', '13:00-AM')).toThrow('The hour must be between 0 and 12');
   });
 
   it('Para os argumentos `Tuesday` e `09:60-AM` deve lançar uma exceção com a mensagem: The minutes must be between 0 and 59', () => {
-    expect(getOpeningHours('Tuesday', '09:60-AM')).toThrow(Error('The minutes must be between 0 and 59'));
+    expect(() => getOpeningHours('Tuesday', '09:60-AM')).toThrow('The minutes must be between 0 and 59');
   });
 });
