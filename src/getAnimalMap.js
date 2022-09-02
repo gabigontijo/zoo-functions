fa168c75833b274a5df019f47fbe837b2367a028
Gabriela Gontijo <dev.gabigontijo@gmail.com>
2022-09-02 12:57:14 +0100

    requesito 14 iniciando

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,29 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+// const includesNamesOnly = ((parametro) =>{} );
+
+// const includesNamesAndSex = ((parametro) =>{});
+
+// const sort = ((parametro) => {})
+
+const parametroUndefinid = () => {
+  return species.map((location) => location.location).reduce((acc, curr) => {
+    acc[curr] = species.filter((animal) => animal.location === curr).map((animal) => animal.name);
+    return acc}, {})
+
+}
+
 function getAnimalMap(options) {
+  if (options === undefined) {
+    return parametroUndefinid();
+  }
   // seu código aqui
 }
 
+// console.log(species.map((location) => location.location).reduce((acc, curr) => {
+// acc[curr] = species.filter((animal) => animal.location === curr).map((animal) => animal.name);
+// return acc}, {}) )
+
+console.log(getAnimalMap());
 module.exports = getAnimalMap;
