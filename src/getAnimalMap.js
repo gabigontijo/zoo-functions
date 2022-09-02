7a89f19d977f4265cec5103b3f8238e862fd3fd2
Gabriela Gontijo <dev.gabigontijo@gmail.com>
2022-09-02 18:42:59 +0100

    requesito 14 3 primeiros

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,29 +1,50 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-// const includesNamesOnly = ((parametro) =>{} );
+const locations = species.map((location) => location.location).reduce((acc, curr) => {
+  if (!acc.some((loc) => loc === curr)) {
+    acc.push(curr);
+  }
+  return acc;
+}, [])
+
+const includesNamesOnly = () => {
+  return locations.reduce((acc, curr) => {
+    const animals = species.filter((animal) => animal.location === curr).
+      flatMap((animal) => animal.name);
+    animals.forEach((wichAnimal) => {
+      if (acc[curr] === undefined) {
+        acc[curr] = [];
+      }
+      obj = {};
+      obj[wichAnimal] = species.find((name) => name.name === wichAnimal).residents.
+        flatMap((name) => name.name);
+      acc[curr].push(obj);
+    })
+    return acc;
+  }, {})
+ }
 
 // const includesNamesAndSex = ((parametro) =>{});
 
 // const sort = ((parametro) => {})
 
 const parametroUndefinid = () => {
-  return species.map((location) => location.location).reduce((acc, curr) => {
+  const objparametroUndefinid = species.map((location) => location.location).reduce((acc, curr) => {
     acc[curr] = species.filter((animal) => animal.location === curr).map((animal) => animal.name);
-    return acc}, {})
-
-}
+    return acc;
+  }, {});
+  return objparametroUndefinid;
+};
 
 function getAnimalMap(options) {
-  if (options === undefined) {
+  if (options === undefined || options.includeNames === undefined) {
     return parametroUndefinid();
   }
-  // seu código aqui
+  if (options.includeNames !== undefined) {
+    return includesNamesOnly();
+  }
 }
+console.log(getAnimalMap({ includeNames: true }))
 
-// console.log(species.map((location) => location.location).reduce((acc, curr) => {
-// acc[curr] = species.filter((animal) => animal.location === curr).map((animal) => animal.name);
-// return acc}, {}) )
-
-console.log(getAnimalMap());
 module.exports = getAnimalMap;
